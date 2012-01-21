c491da79c8926446612848777434924fdee76a40
Enno Boland (Gottox) <g@s01.de>
2012-01-21 23:29:50 +0100

    Adding doc to IOAcknowledge

--- src/io/socket/IOAcknowledge.java
+++ src/io/socket/IOAcknowledge.java
@@ -1,5 +1,22 @@
+/*
+ * socket.io-java-client IOAcknowledge.java
+ *
+ * Copyright (c) 2012, Enno Boland
+ * PROJECT DESCRIPTION
+ * 
+ * See LICENSE file for more information
+ */
 package io.socket;
 
+/**
+ * The Interface IOAcknowledge.
+ */
 public interface IOAcknowledge {
+	
+	/**
+	 * Acknowledges a package
+	 *
+	 * @param args the args
+	 */
 	void ack(Object... args);
 }
