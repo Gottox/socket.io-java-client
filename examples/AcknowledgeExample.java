e199b1e55efcf8d9e32abc0a32220bd7f61a9083
Enno Boland (Gottox) <g@s01.de>
2012-02-28 21:24:48 +0100

    replacing examples

---
+++ examples/AcknowledgeExample.java
@@ -0,0 +1,70 @@
+/*
+ * socket.io-java-client Test.java
+ *
+ * Copyright (c) 2012, Enno Boland
+ * socket.io-java-client is a implementation of the socket.io protocol in Java.
+ * 
+ * See LICENSE file for more information
+ */
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
+
+import org.json.JSONObject;
+
+public class AcknowledgeExample implements IOCallback {
+	private SocketIO socket;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			new AcknowledgeExample();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public AcknowledgeExample() throws Exception {
+		socket = new SocketIO();
+		socket.connect("http://127.0.0.1:3001/", this);
+
+		// Sends a string to the server.
+		socket.send(new IOAcknowledge() {
+			@Override
+			public void ack(Object... args) {
+				System.out.println("Server acknowledges this package.");
+				for(Object o : args)
+					System.out.println(o.toString());
+			}
+		}, "Hello Server");
+	}
+
+	@Override
+	public void onMessage(JSONObject json, IOAcknowledge ack) {
+	}
+
+	@Override
+	public void onMessage(String data, IOAcknowledge ack) {
+		// acknowledges a package
+		ack.ack("argument1", 23.42, "argument3");
+	}
+
+	@Override
+	public void onError(SocketIOException socketIOException) {
+	}
+
+	@Override
+	public void onDisconnect() {
+	}
+
+	@Override
+	public void onConnect() {
+	}
+
+	@Override
+	public void on(String event, IOAcknowledge ack, Object... args) {
+	}
+}
---
+++ examples/BasicExample.java
@@ -0,0 +1,80 @@
+/*
+ * socket.io-java-client Test.java
+ *
+ * Copyright (c) 2012, Enno Boland
+ * socket.io-java-client is a implementation of the socket.io protocol in Java.
+ * 
+ * See LICENSE file for more information
+ */
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BasicExample implements IOCallback {
+	private SocketIO socket;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			new BasicExample();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public BasicExample() throws Exception {
+		socket = new SocketIO();
+		socket.connect("http://127.0.0.1:3001/", this);
+
+		// Sends a string to the server.
+		socket.send("Hello Server");
+
+		// Sends a JSON object to the server.
+		socket.send(new JSONObject().put("key", "value").put("key2",
+				"another value"));
+
+		// Emits an event to the server.
+		socket.emit("event", "argument1", "argument2", 13.37);
+	}
+
+	@Override
+	public void onMessage(JSONObject json, IOAcknowledge ack) {
+		try {
+			System.out.println("Server said:" + json.toString(2));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onMessage(String data, IOAcknowledge ack) {
+		System.out.println("Server said: " + data);
+	}
+
+	@Override
+	public void onError(SocketIOException socketIOException) {
+		System.out.println("an Error occured");
+		socketIOException.printStackTrace();
+	}
+
+	@Override
+	public void onDisconnect() {
+		System.out.println("Connection terminated.");
+	}
+
+	@Override
+	public void onConnect() {
+		System.out.println("Connection established");
+	}
+
+	@Override
+	public void on(String event, IOAcknowledge ack, Object... args) {
+		System.out.println("Server triggered event '" + event + "'");
+	}
+}
