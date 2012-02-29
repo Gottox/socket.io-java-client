cdcb5f263d4527031769ed890a0e06cd068d2cdb
Enno Boland (Gottox) <g@s01.de>
2012-02-29 09:21:34 +0100

    Adding MutateProxy to test non-valid behavior of socket.io

---
+++ tests/io/socket/testutils/MutateProxy.java
@@ -0,0 +1,12 @@
+package io.socket.testutils;
+
+public class MutateProxy extends Thread {
+	public MutateProxy() {
+		super("MutateProxy");
+	}
+
+	@Override
+	public void run() {
+		super.run();
+	}
+}
