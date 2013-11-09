13d1a79f6e21d02523a6ddbe1e1d146809901209
refaelos <refaeldakar@gmail.com>
2013-11-09 18:09:40 +0200

    Not allowing all certificates

--- src/io/socket/WebsocketTransport.java
+++ src/io/socket/WebsocketTransport.java
@@ -3,6 +3,9 @@ package io.socket;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.regex.Pattern;
 
 import javax.net.ssl.SSLContext;
@@ -28,28 +31,49 @@ class WebsocketTransport extends WebSocketClient implements IOTransport {
     }
 
 	public WebsocketTransport(URI uri, IOConnection connection) {
+//        super(uri);
+//        this.connection = connection;
+//        try {
+//			X509TrustManager tm = new X509TrustManager() {
+//	            public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+//	            }
+//
+//	            public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+//	            }
+//
+//	            public X509Certificate[] getAcceptedIssuers() {
+//	                return null;
+//	            }
+//	        };
+//	        SSLContext ctx = SSLContext.getInstance("TLS");
+//            ctx.init(null, null, null);
+//
+//	        if(("wss".equals(uri.getScheme()) || ("https".equals(uri.getScheme()))) && ctx != null) {
+//                this.setSocket(ctx.getSocketFactory().createSocket());
+//	        }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
         super(uri);
         this.connection = connection;
+        SSLContext context = null;
         try {
-			X509TrustManager tm = new X509TrustManager() {
-	            public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-	            }
-
-	            public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-	            }
-
-	            public X509Certificate[] getAcceptedIssuers() {
-	                return null;
-	            }
-	        };
-	        SSLContext ctx = SSLContext.getInstance("TLS");
-            ctx.init(null, new TrustManager[]{tm}, null);
-			
-	        if(("wss".equals(uri.getScheme()) || ("https".equals(uri.getScheme()))) && ctx != null) {
-                this.setSocket(ctx.getSocketFactory().createSocket());
-	        }
-        } catch (Exception e) {
+            context = SSLContext.getInstance("TLS");
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
+        }
+        try {
+            context.init(null, null, null);
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+        if("wss".equals(uri.getScheme()) && context != null) {
+            try {
+                this.setSocket(context.getSocketFactory().createSocket());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
