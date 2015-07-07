340f6d852b9dd7bfd1286f615c6dc88ae2705d20
Cristina De Rito <artemisia-absynthium@users.noreply.github.com>
2015-07-07 16:06:53 +0200

    use default provider since Harmony is no longer supported on Lollipop

--- src/io/socket/WebsocketTransport.java
+++ src/io/socket/WebsocketTransport.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.util.regex.Pattern;
 
 import javax.net.ssl.SSLContext;
@@ -15,96 +14,102 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 class WebsocketTransport extends WebSocketClient implements IOTransport {
-    private final static Pattern PATTERN_HTTP = Pattern.compile("^http");
-    public static final String TRANSPORT_NAME = "websocket";
-    private IOConnection connection;
-    public static IOTransport create(URL url, IOConnection connection) {
-        URI uri = URI.create(
-                PATTERN_HTTP.matcher(url.toString()).replaceFirst("ws")
-                + IOConnection.SOCKET_IO_1 + TRANSPORT_NAME
-                + "/" + connection.getSessionId());
+	private final static Pattern PATTERN_HTTP = Pattern.compile("^http");
+	public static final String TRANSPORT_NAME = "websocket";
+	private IOConnection connection;
 
-        return new WebsocketTransport(uri, connection);
-    }
+	public static IOTransport create(URL url, IOConnection connection) {
+		URI uri = URI.create(PATTERN_HTTP.matcher(url.toString()).replaceFirst("ws") + IOConnection.SOCKET_IO_1
+				+ TRANSPORT_NAME + "/" + connection.getSessionId());
+
+		return new WebsocketTransport(uri, connection);
+	}
 
 	public WebsocketTransport(URI uri, IOConnection connection) {
 		super(uri);
 		this.connection = connection;
 		SSLContext context = null;
 		try {
-			context = SSLContext.getInstance("TLS", "HarmonyJSSE");
+			context = SSLContext.getInstance("TLS");
 			context.init(null, null, null);
 			if ("wss".equals(uri.getScheme())) {
 				this.setSocket(SSLSocketFactory.getDefault().createSocket());
 			}
-		} catch (NoSuchAlgorithmException | NoSuchProviderException
-				| KeyManagementException | IOException e) {
+		} catch (NoSuchAlgorithmException | KeyManagementException | IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-    /* (non-Javadoc)
-     * @see io.socket.IOTransport#disconnect()
-     */
-    @Override
-    public void disconnect() {
-        try {
-            this.close();
-        } catch (Exception e) {
-            connection.transportError(e);
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.socket.IOTransport#disconnect()
+	 */
+	@Override
+	public void disconnect() {
+		try {
+			this.close();
+		} catch (Exception e) {
+			connection.transportError(e);
+		}
+	}
 
-    /* (non-Javadoc)
-     * @see io.socket.IOTransport#canSendBulk()
-     */
-    @Override
-    public boolean canSendBulk() {
-        return false;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.socket.IOTransport#canSendBulk()
+	 */
+	@Override
+	public boolean canSendBulk() {
+		return false;
+	}
 
-    /* (non-Javadoc)
-     * @see io.socket.IOTransport#sendBulk(java.lang.String[])
-     */
-    @Override
-    public void sendBulk(String[] texts) throws IOException {
-        throw new RuntimeException("Cannot send Bulk!");
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.socket.IOTransport#sendBulk(java.lang.String[])
+	 */
+	@Override
+	public void sendBulk(String[] texts) throws IOException {
+		throw new RuntimeException("Cannot send Bulk!");
+	}
 
-    /* (non-Javadoc)
-     * @see io.socket.IOTransport#invalidate()
-     */
-    @Override
-    public void invalidate() {
-        connection = null;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.socket.IOTransport#invalidate()
+	 */
+	@Override
+	public void invalidate() {
+		connection = null;
+	}
 
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-        if(connection != null)
-            connection.transportDisconnected();
-    }
+	@Override
+	public void onClose(int code, String reason, boolean remote) {
+		if (connection != null)
+			connection.transportDisconnected();
+	}
 
-    @Override
-    public void onMessage(String text) {
-        if(connection != null)
-            connection.transportMessage(text);
-    }
+	@Override
+	public void onMessage(String text) {
+		if (connection != null)
+			connection.transportMessage(text);
+	}
 
-    @Override
-    public void onOpen(ServerHandshake handshakedata) {
-        if(connection != null)
-            connection.transportConnected();
-    }
+	@Override
+	public void onOpen(ServerHandshake handshakedata) {
+		if (connection != null)
+			connection.transportConnected();
+	}
 
-    @Override
-    public String getName() {
-        return TRANSPORT_NAME;
-    }
+	@Override
+	public String getName() {
+		return TRANSPORT_NAME;
+	}
 
-    @Override
-    public void onError(Exception ex) {
-        // TODO Auto-generated method stub
+	@Override
+	public void onError(Exception ex) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 }
